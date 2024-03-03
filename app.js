ba0c289098980b3dca1c0cd0dc7437a0ae3c49e9
thakurlakshay2 <thakurlakshay2@gmail.com>
2024-03-03 12:00:40 +0530

    Basic authentication

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const csrf = require("csurf");
+const flash = require("connect-flash");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 
@@ -17,6 +18,7 @@ const store = new MongoDBStore({
   uri: MONGO_DB_URI,
   collection: "sessions",
 });
+// const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -35,8 +37,13 @@ app.use(
     store: store,
   })
 );
+// app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
+  // if (!req.session.user) {
+  //   return next();
+  // }
   User.findById("5bab316ce0a7c75f783cb8a8")
     .then((user) => {
       req.user = user;
@@ -44,7 +51,12 @@ app.use((req, res, next) => {
     })
     .catch((err) => console.log(err));
 });
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = "";
 
+  next();
+});
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -54,18 +66,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGO_DB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Max",
-          email: "max@test.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,79 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  console.log(req.get("Cookie"));
-  // const isLoggedIn = req.get("Cookie").trim().split("=")[1];
-  const isLoggedIn = req.session.isLoggedIn;
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
+    errorMessage: req.flash("err"),
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
-    .then((user) => {
-      req.user = user;
-      req.session.isLoggedIn = true;
-      res.redirect("/");
+  const email = req.body.email;
+  const pass = req.body.password;
+  User.findOne({ email: email }).then((user) => {
+    if (!user) {
+      req.flash("err", "Invalid email or password");
+      return res.redirect("/login");
+    }
+    bcrypt
+      .compare(pass, user.password)
+      .then((result) => {
+        if (result) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            res.redirect("/");
+          });
+        }
+        req.flash("err", "Invalid email or password");
+        res.redirect("/login");
+      })
+      .catch((err) => {
+        res.redirect("/login");
+      });
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const pass = req.body.password;
+  const confirmPass = req.body.confirmPassword;
+  console.log(email);
+  User.findOne({ email: email })
+    .then((useDoc) => {
+      if (useDoc) {
+        return res.redirect("/signup");
+      }
+      return bcrypt.hash(pass, 12).then((hashedPass) => {
+        const user = new User({
+          name: email,
+          email: email,
+          password: hashedPass,
+          cart: { items: [] },
+        });
+        return user.save();
+      });
     })
-    .catch((err) => console.log(err));
-  // res.setHeader("Set-Cookie", "loggedIn=true");
+    .then(() => {
+      res.redirect("/login");
+    })
+    .catch((err) => {});
 };
 
 exports.postLogout = (req, res, next) => {
-  // res.setHeader("Set-Cookie", "loggedIn=true");
-  req.session.destroy(() => {
+  req.session.destroy((err) => {
+    console.log(err);
     res.redirect("/");
   });
 };
